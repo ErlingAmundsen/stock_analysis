19156a40b0655f3a39afed85b1f05fbe29213f15
Erling Amundsen <erling.amundsen@gmail.com>
2024-01-11 19:11:56 +0100

    Tested around with new tools

--- analysis/testing.py
+++ analysis/testing.py
@@ -1,74 +1,41 @@
 import sys
-import plotly.graph_objects as ply
-import datetime
+from dataclasses import dataclass
 
-sys.path.append('./')
-
+sys.path.append("./")
 from main_functions import format_yahoo as fy
 
-def candle_plot(data, meta = None,title = None, xtitle = None, ytitle = None):
-    
 
-    # Chose ply because of integrated interactability (slide window and cool colors)
+def testing(vals):
+    list_test = vals.copy()
 
-    plotly_data = [ply.Candlestick(
-        x = data.index,
-        open= data['Open'],
-        high= data['High'],
-        low= data['Low'],
-        close= data['Close']
-    )]
-
-    fig = ply.Figure(data = plotly_data)
-    
-
-    # making my plot look cool because cant fool around
-    # with stock data with ugly looking plots, that makes it so much worse
-    bg = 'black'
-    text = 'green'
-    grid = 'green'
-  
-
-    layout_dict = {'paper_bgcolor': bg,
-                   'plot_bgcolor': bg,
-                   'title': {'font': {'color' : text}},
-                   'xaxis_title':{'font': {'color' : text}},
-                   'yaxis_title':{'font': {'color' : text}},
-
-                   # green grids is mega cool
-                   'xaxis' : {'gridcolor': grid,
-                              'tickfont': {'color':grid}},
-
-                   'yaxis' : {'gridcolor': grid,
-                              'tickfont': {'color':grid}}
-                
-                   }
-    
-    if title:
-        layout_dict['title']['text'] = title
-    else:
-        layout_dict['title']['text'] = f'{meta['Ticker']} stock from {datetime.date.strftime(meta['Start'], '%b/%y')} to {datetime.date.strftime(meta['End'], '%b/%y')}'
-    
-    if xtitle:
-        layout_dict['xaxis_title']['text'] = xtitle
-    else:
-        layout_dict['xaxis_title']['text'] = 'Date'
+    for i in range(len(list_test)):
+        list_test[i] = min(0, list_test[i])
+    return list_test
 
 
-    if ytitle:
-        layout_dict['yaxis_title']['text'] = ytitle
-    else:
-        layout_dict['yaxis_title']['text'] = 'Price'
+@dataclass
+class test_class:
+    _name: str
 
-    fig.update_layout(layout_dict)
+    def __post_init__(self):
+        self._name = self._name.upper()
 
-    return fig
+    @property
+    def name_t(self):
+        return self._name
+
+    @name_t.setter
+    def name_t(self, val):
+        self._name = val.upper()
 
 
-if __name__ == '__main__':
-    data, meta = fy.format_yahoo('BTC-USD', period= '1y')
-    fig = candle_plot(data, meta)
-    title = 'Title test'
+if __name__ == "__main__":
+    data, meta = fy.format_yahoo("BTC-USD", period="1y")
 
-    fig.show()
-    
+    vals = "hello"
+    cls = test_class(vals)
+    print(cls._name)
+    print(cls.name_t)
+    cls.name_t = "not_hello"
+    print(cls.name_t)
+    cls.name_t = "Nnonon"
